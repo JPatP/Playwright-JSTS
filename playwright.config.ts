6ab97cbba23a43f2ec6abd0045e2521e79a774b6
JPatP <gonzaless424@gmail.com>
2026-06-02 17:38:51 +0800

    Updated UIBasics

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,13 +2,13 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-  timeout: 40 * 1000,
+  timeout: 30 * 1000,
   
   expect: {
-    timeout: 40 * 1000,
+    timeout: 5000,
   },
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [["html", { open: "always"}]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     browserName: 'chromium',
--- tests/UIBasicsTest.spec.js
+++ tests/UIBasicsTest.spec.js
@@ -1,19 +1,28 @@
 const { test } = require("@playwright/test");
 
-test("create page via browser.newContext and open login page", async ({
+test.only("create page via browser.newContext and open login page", async ({
   browser,
 }) => {
   const context = await browser.newContext();
   const page = await context.newPage();
   await page.goto("https://www.rahulshettyacademy.com/loginpagePractise/");
-//   const title = await page.title();
-//   console.log(title);
+  const title = await page.title();
+  console.log(title);
+  // css,xpath
+  await page.locator("input#username").fill("admin");
+  await page.locator("input#password").fill("password");
+  await page.locator("#signInBtn").click();
+  // wait untl this locator is visible, but in playwright no need to write, 
+  // it will automatically wait for the element to be visible before performing any action on it.
+  const errorMessage = await page.locator("[style*='block']").textContent();
+  console.log(errorMessage);
 });
 
 test("use fixture page to open Google", async ({ page }) => {
   await page.goto("https://www.google.com/");
   // get title and assert
-//   const title = await page.title();
-//   console.log(title);
-//   expect(page).toHaveTitle("Google");
+  const title = await page.title();
+  console.log(title);
+  expect(page).toHaveTitle("Google");
+
 });
