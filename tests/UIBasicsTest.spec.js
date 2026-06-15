b8a9c8d519b230d33003dc9063026ed14f446fa7
JPatP <gonzaless424@gmail.com>
2026-06-15 08:53:57 +0800

    Update

--- tests/UIBasicsTest.spec.js
+++ tests/UIBasicsTest.spec.js
@@ -1,6 +1,29 @@
-const { test,expect } = require("@playwright/test");
+const { test, expect } = require("@playwright/test");
 
-test.only("create page via browser.newContext and open login page", async ({
+test("create page via browser.newContext and open Client App", async ({
+  browser,
+}) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto("https://www.rahulshettyacademy.com/client", {
+    waitUntil: "domcontentloaded",
+  });
+  const title = await page.title();
+  console.log(title);
+
+  await page.locator("input#userEmail").fill("gonzaless424@gmail.com");
+  await page.locator("input#userPassword").fill("Automation@2026");
+  await page.locator("#login").click();
+
+  await page.locator(".card-body b").last().waitFor();
+  const products = await page.locator(".card-body b").allTextContents();
+
+  console.log(products);
+
+  // await page.pause();
+});
+
+test("create page via browser.newContext and open loginpagePractise", async ({
   browser,
 }) => {
   const context = await browser.newContext();
@@ -9,16 +32,43 @@ test.only("create page via browser.newContext and open login page", async ({
   const title = await page.title();
   console.log(title);
   // css,xpath
-  await page.locator("input#username").fill("admin");
-  await page.locator("input#password").fill("password");
-  await page.locator("#signInBtn").click();
-  // wait untl this locator is visible, but in playwright no need to write, 
+  const username = page.locator("input#username");
+  const password = page.locator("input#password");
+  const signInBtn = page.locator("#signInBtn");
+
+  await username.fill("admin");
+  await password.fill("password");
+
+  signInBtn.click();
+  // wait untl this locator is visible, but in playwright no need to write,
   // it will automatically wait for the element to be visible before performing any action on it.
-  const errorMessageLocator = await page.locator("[style*='block']")
+  const errorMessageLocator = await page.locator("[style*='block']");
   const errorMessage = await errorMessageLocator.textContent();
   console.log(errorMessage);
-  await expect(errorMessageLocator).toContainText("Hahah");
+  await expect(errorMessageLocator).toContainText(errorMessage);
+
+  await username.fill("");
+  await username.fill("rahulshettyacademy");
+  await password.fill("");
+  await password.fill("Learning@830$3mK2");
+  await signInBtn.click();
+
+  const cardTitles = page.locator(".card-body a");
+  // get the text of the first link in the card body and print it
+  console.log(await cardTitles.first().textContent());
+  // get the text of the second link in the card body and print it
+  console.log(await cardTitles.nth(1).textContent());
+  const allTitles = await cardTitles.allTextContents();
+  console.log(allTitles);
+
+  // assertions
+  await expect(cardTitles.first()).toContainText("iphone");
+  await expect(cardTitles.nth(1)).toContainText("Samsung");
+
+  // await page.pause()
 });
+
+
 
 test("use fixture page to open Google", async ({ page }) => {
   await page.goto("https://www.google.com/");
@@ -26,5 +76,36 @@ test("use fixture page to open Google", async ({ page }) => {
   const title = await page.title();
   console.log(title);
   expect(page).toHaveTitle("Google");
+});
 
+
+test.only("Open loginpagePractise and test UI Controls", async ({
+  browser,
+}) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto("https://www.rahulshettyacademy.com/loginpagePractise/");
+  const title = await page.title();
+  console.log(title);
+  // css,xpath
+  const username = page.locator("input#username");
+  const password = page.locator("input#password");
+  const signInBtn = page.locator("#signInBtn");
+
+
+  await username.fill("rahulshettyacademy");
+  await password.fill("Learning@830$3mK2");
+  await signInBtn.click();
+
+  const dropdown = page.locator("select.form-control");
+  await dropdown.selectOption("consult");
+
+  await page.locator(".radiotextsty").last().click();
+  await page.locator("#okayBtn").click();
+
+  
+  console.log(await page.locator(".radiotextsty").last().isChecked());
+  await expect(page.locator(".radiotextsty").last()).toBeChecked();
+
+  await page.pause()
 });
